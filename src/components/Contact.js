728a0ef9b18249adff593f2de75643d834717ace
Satnammeena2004 <satnam.meena2004@gmail.com>
2024-08-03 23:42:59 +0530

    refactor code

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,15 +1,10 @@
 import {Outlet} from "react-router-dom";
 
 const Contact = () => {
-  // const param = useParams();
-  // const location = useLocation();
-  // const match = useMatches();
-  // const  [getItems,setItems,showAllItems,seeAllKeys,seeAllValues] = useStorage();
+
 
   console.log("contact Rendring 🚀");
-  // console.log(match);
-  // console.log(location);
-  // console.log(param);
+
   return (
     <>
       <h1>Contact Us Page : </h1>
